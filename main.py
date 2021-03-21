f58d0caa443b06785214e782b66c2686e51d30ce
Eduardo Villani <villani.ed@gmail.com>
2021-03-21 15:54:03 -0300

    item2vec final version

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import warnings
 from typing import Union, List
 
 import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+from recommenders.item2vec.item2vec_recommender import Item2VecRecommender
+from recommenders.utils.train_test_and_validation import TrainTestAndValidation
 
 try:
     from LibRecommender.libreco.algorithms import SVDpp
@@ -43,30 +48,43 @@ class CoreDataRecipe(DataReaderBase):
 
 
 def main():
+    # rdi = RawDataInteraction()
+    #
+    # train_data, eval_data, test_data = random_split(rdi(), multi_ratios=[0.64, 0.16, 0.2])
+    # train_data, data_info = DatasetPure.build_trainset(train_data)
+    # eval_data = DatasetPure.build_evalset(eval_data)
+    # test_data = DatasetPure.build_testset(test_data)
+    # print(data_info)
+    #
+    # svdpp = SVDpp(
+    #     task="rating",
+    #     data_info=data_info,
+    #     embed_size=16,
+    #     n_epochs=5,
+    #     lr=0.001,
+    #     reg=None,
+    #     batch_size=256
+    # )
+    #
+    # svdpp.fit(train_data, verbose=2, eval_data=eval_data, metrics=["rmse", "mae", "r2"])
+    # svdpp.save(
+    #     path='models/rec_sys_project_cf',
+    #     model_name='rec_sys_project_cf')
+    # # do final evaluation on test data
+    # print("evaluate_result: ", evaluate(model=svdpp, data=test_data, metrics=["rmse", "mae"]))
+
     rdi = RawDataInteraction()
-
-    train_data, eval_data, test_data = random_split(rdi(), multi_ratios=[0.64, 0.16, 0.2])
-    train_data, data_info = DatasetPure.build_trainset(train_data)
-    eval_data = DatasetPure.build_evalset(eval_data)
-    test_data = DatasetPure.build_testset(test_data)
-    print(data_info)
-
-    svdpp = SVDpp(
-        task="rating",
-        data_info=data_info,
-        embed_size=16,
-        n_epochs=5,
-        lr=0.001,
-        reg=None,
-        batch_size=256
+    train_test_and_validation = TrainTestAndValidation(rdi(), 0.2)
+    item2vec_recommender = Item2VecRecommender(
+        train_test_and_validation.train,
+        load_model='models/item2vec_embeddings_rate_ge_4',
+        text_type='recipe'
     )
-
-    svdpp.fit(train_data, verbose=2, eval_data=eval_data, metrics=["rmse", "mae", "r2"])
-    svdpp.save(
-        path='models/rec_sys_project_cf',
-        model_name='rec_sys_project_cf')
-    # do final evaluation on test data
-    print("evaluate_result: ", evaluate(model=svdpp, data=test_data, metrics=["rmse", "mae"]))
+    item2vec_recommender.evaluate(train_test_and_validation.test)
+    y_test = np.array(train_test_and_validation.test['label'])
+    y_pred = item2vec_recommender.y_pred
+    print(f"RMSE: {mean_squared_error(y_test, y_pred, squared=False)}")
+    print(f"MSE: {mean_squared_error(y_test, y_pred, squared=True)}")
 
 
 if __name__ == '__main__':
---
+++ recommenders/item2vec/item2vec_recommender.py
@@ -0,0 +1,76 @@
+import numpy as np
+from typing import Union
+
+from recommenders.item2vec.item2vec import Item2Vec
+from pandas import DataFrame
+from tqdm import tqdm
+
+
+class Item2VecRecommender:
+    def __init__(self, data: DataFrame, load_model: Union[None, str] = None, save_path: Union[None, str] = None, text_type: str = 'recipe'):
+        self._data = data
+        self._solution = None
+        self._text_type = text_type
+        if load_model is not None:
+            from gensim.models import Word2Vec
+            self._model = Word2Vec.load(load_model)
+            self._word_vectors = self._model.wv
+            item2vec = Item2Vec(data)
+        else:
+            item2vec = Item2Vec(data)
+            item2vec.train(text_type=text_type)
+            if save_path is not None:
+                item2vec.save(save_path)
+            self._model = item2vec.model
+            self._word_vectors = self._model.wv
+
+        self._user2index = item2vec.user2index
+        self._index2user = item2vec.index2user
+
+        self._recipe2index = item2vec.recipe2index
+        self._index2recipe = item2vec.index2recipe
+
+        self._user_recipe_dict = item2vec.user_recipe
+        self._recipe_user_dict = item2vec.recipe_user
+        self._user_mean_rate = item2vec.user_mean_rate
+        self._item_mean_rate = item2vec.item_mean_rate
+
+    def evaluate(self, test):
+        def similarities(key, subkey, main_dict):
+            similarities_list = []
+            rates = []
+            for slave_key in main_dict[key]:
+                similarities_list.append(self.word_vector.similarity(str(subkey), str(slave_key[0])))
+                rates.append(slave_key[1])
+            rates = np.array(rates, dtype=np.float32)
+            similarities_list = np.array(similarities_list, dtype=np.float32)
+            return np.average(rates, weights=similarities_list)
+        self._solution = np.zeros(len(test))
+        global_mean = np.mean(self._data['label'])
+        for index, row in tqdm(enumerate(np.array(test))):
+            user = row[0]
+            recipe = row[1]
+            data_dict = self._user_recipe_dict if self._text_type == 'recipe' else self._recipe_user_dict
+            if user in self._user2index and recipe in self._recipe2index:
+                key = recipe if self._text_type == 'user' else user
+                subkey = user if self._text_type == 'user' else recipe
+                self._solution[index] = similarities(key, subkey, data_dict)
+            elif user in self._user2index and recipe not in self._recipe2index:
+                self._solution[index] = self._user_mean_rate[user]
+            elif user not in self._user2index and recipe in self._recipe2index:
+                self._solution[index] = self._item_mean_rate[recipe]
+            else:
+                self._solution[index] = global_mean
+
+    @property
+    def model(self):
+        return self._model
+
+    @property
+    def word_vector(self):
+        return self._word_vectors
+
+    @property
+    def y_pred(self):
+        return self._solution
+
